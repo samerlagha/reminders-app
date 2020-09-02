5d44bab04ceab3dcb227bea4c45081bc0c72f7f7
samerlagha <samer_la@mail.ru>
2020-09-02 11:50:50 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import {createStore} from 'redux'
+import {Provider}   from 'react-redux'
+import App from './components/App';
+import reminders from './reducers';
+import './index.css'
 
+const store= createStore(reminders)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <Provider store={store}>
+        <App />
+      </Provider>
+     , document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
